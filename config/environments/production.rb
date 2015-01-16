ec5269646fb15cef965c69d6da7984a3bf1a1e6b
S.V.d.M <svdm@SVdMs-MacBook-Pro.local>
2015-01-16 11:50:50 -0800

    X-Frame header

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,6 +37,12 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
+  config.action_dispatch.default_headers = {
+
+    'X-Frame-Options' => 'ALLOWALL'
+
+  }
+
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
